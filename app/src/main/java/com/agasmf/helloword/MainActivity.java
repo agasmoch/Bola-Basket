e32c955db2f30c20ec9115a21a7899a2a10d99b6
agasmoch <agasm5@gmail.com>
2019-10-16 13:03:04 +0700

    Commit ke3

--- app/src/main/java/com/agasmf/helloword/MainActivity.java
+++ app/src/main/java/com/agasmf/helloword/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int scorerujaktambah=0;
+    private int scoretumpengtambah=0;
+    private int reset=0;
 
 
     @Override
@@ -22,5 +24,44 @@ public class MainActivity extends AppCompatActivity {
         textViewRujak.setText(String.valueOf(scorerujaktambah));
     }
 
+    public void tambah2pointrujak(View view) {
+        scorerujaktambah = scorerujaktambah +2;
+        TextView textViewRujak = findViewById(R.id.scorerujak);
+        textViewRujak.setText(String.valueOf(scorerujaktambah));
     }
+
+    public void tambah1pointrujak(View view) {
+            scorerujaktambah = scorerujaktambah +1;
+            TextView textViewRujak = findViewById(R.id.scorerujak);
+            textViewRujak.setText(String.valueOf(scorerujaktambah));
+    }
+
+    public void tambah1pointtumpeng(View view) {
+        scoretumpengtambah = scoretumpengtambah+1;
+        TextView textViewTumpeng=findViewById(R.id.scoretumpeng);
+        textViewTumpeng.setText(String.valueOf(scoretumpengtambah));
+    }
+
+    public void tambah2pointtumpeng(View view) {
+        scoretumpengtambah = scoretumpengtambah+2;
+        TextView textViewTumpeng=findViewById(R.id.scoretumpeng);
+        textViewTumpeng.setText(String.valueOf(scoretumpengtambah));
+    }
+
+    public void tambah3pointtumpeng(View view) {
+        scoretumpengtambah = scoretumpengtambah+3;
+        TextView textViewTumpeng=findViewById(R.id.scoretumpeng);
+        textViewTumpeng.setText(String.valueOf(scoretumpengtambah));
+    }
+
+    public void reset(View view) {
+        reset=scorerujaktambah=0;
+        reset=scoretumpengtambah=0;
+        TextView textViewresettumpeng=findViewById(R.id.scoretumpeng);
+        TextView textViewresetrujak=findViewById(R.id.scorerujak);
+        textViewresettumpeng.setText(String.valueOf(reset));
+        textViewresetrujak.setText(String.valueOf(reset));
+
+    }
+}
 
